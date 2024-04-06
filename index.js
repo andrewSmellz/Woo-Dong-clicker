88864f4349d109f3e9329a068a8c813140d8c327
andrewSmellz <andrewnaquino@gmail.com>
2024-04-06 18:18:41 -0400

    now it saves how many times you clicked :D

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-let timesClicked=0;
+// Initial value for timesClicked
+let timesClicked = 0;
 
-function clickWooDong(){
+// Function to update the timesClicked text
+function clickWooDong() {
     timesClicked++;
-    console.log(timesClicked);
-    document.getElementById("timesClicked").innerHTML= `you have clicked Woo-Dong ${timesClicked} times`;
+    document.getElementById("timesClicked").innerHTML = `You have clicked Woo-Dong ${timesClicked} times`;
+}
+
+// Function to save the value of timesClicked to a cookie
+function save() {
+    setCookie("timesClicked", timesClicked, 7); // Save timesClicked to a cookie with a 7-day expiry
+}
+
+// Function to retrieve the value of timesClicked from a cookie and update the timesClicked variable and paragraph
+function load() {
+    let retrievedValue = getCookie("timesClicked");
+    if (retrievedValue !== null) {
+        timesClicked = parseInt(retrievedValue);
+        document.getElementById("timesClicked").innerHTML = `You have clicked Woo-Dong ${timesClicked} times`;
+    } else {
+        alert("Cookie not found or expired");
+    }
+    console.log(getCookie("timesClicked"));
+}
+
+// Function to set a cookie
+function setCookie(name, value, daysToExpire) {
+    let expires = "";
+    if (daysToExpire) {
+        const date = new Date();
+        date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Function to get a cookie value
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
 }
